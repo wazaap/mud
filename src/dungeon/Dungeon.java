19f158967692a72cc5a06c801242b2086cb32ae8
Mads <Mads@10.99.17.218>
2013-01-09 12:08:51 +0100

    New classes are created.

---
+++ src/dungeon/Dungeon.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dungeon;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Thomas
+ */
+public class Dungeon {
+    private ArrayList<Room> rooms = new ArrayList();
+
+    public Dungeon() {
+        
+    } 
+    
+    public void addRoom(Room newRoom) {
+        rooms.add(newRoom);
+    }
+}
