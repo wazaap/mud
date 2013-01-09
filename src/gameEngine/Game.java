b6bdd90b16395b98196792a54045b16c6843ed10
Mads <Mads@10.99.17.218>
2013-01-09 14:02:22 +0100

    Monstre bliver selv genereret i hvert rum.

--- src/gameEngine/Game.java
+++ src/gameEngine/Game.java
@@ -6,6 +6,10 @@ package gameEngine;
 
 import dungeon.Dungeon;
 import fileio.FileIO;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,7 +21,20 @@ public class Game {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Dungeon test = FileIO.readDungeon();
-        System.out.println("");
+        try {
+            Dungeon test;
+                test = FileIO.readDungeon();
+                System.out.println("");
+                
+                
+                
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+
+       
     }
 }
