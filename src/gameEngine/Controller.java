c2991b02cfc81ee52edf706040a6c0db83d453f9
Thomas <Thomas@10.99.17.127>
2013-01-09 12:25:01 +0100

    CurrentRoom Added. Controller added.

---
+++ src/gameEngine/Controller.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameEngine;
+
+import dungeon.Dungeon;
+import fileio.FileIO;
+
+/**
+ *
+ * @author Thomas
+ */
+public class Controller {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Dungeon test = FileIO.readDungeon();
+        System.out.println("");
+    }
+}
--- src/gameEngine/Game.java
+++ src/gameEngine/Game.java
@@ -4,20 +4,10 @@
  */
 package gameEngine;
 
-import dungeon.Dungeon;
-import fileio.FileIO;
-
 /**
  *
  * @author Thomas
  */
 public class Game {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Dungeon test = FileIO.readDungeon();
-        System.out.println("");
-    }
+    
 }
